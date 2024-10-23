950b576669a9a718cbae57c01637bfa6f31e44a6
Brian <mgbrian@users.noreply.github.com>
2024-10-23 19:22:32 +0100

    Add boilerplate app and template code.

---
+++ routes.py
@@ -0,0 +1,19 @@
+import os
+
+from quart import Quart, render_template
+
+import env
+
+
+app = Quart(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
+
+
+@app.route('/', methods=['GET'])
+async def index():
+    return await render_template('index.html')
+
+
+@app.route('/admin', methods=['GET'])
+async def admin():
+    return await render_template('admin.html')
