3c17a37e8867dbbd9e93e81f67ed3a826aee4e23
Brian <mgbrian@users.noreply.github.com>
2024-10-24 06:41:01 +0100

    Correct typo in previous commit.

--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ import os
 from pdf2image import convert_from_path
 
 
-async def save_image(image, output_image_path):
+async def save_image(image, save_path):
     """Save an image asynchronously.
 
     Args:
         image: PIL Image - The Image object to save.
         save_path: str - The path to save the image to.
     """
-    await asyncio.to_thread(image.save, output_image_path, 'PNG')
+    await asyncio.to_thread(image.save, save_path, 'PNG')
 
 
 async def save_pdf_as_images(pdf_path, output_folder, dpi=200):
