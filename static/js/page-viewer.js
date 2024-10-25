bdf6a86fdcd924edf9c62ac4f221fda1d4e37078
Brian <mgbrian@users.noreply.github.com>
2024-10-25 11:38:06 +0100

    Add page display functionality to the document detail page.

---
+++ static/js/page-viewer.js
@@ -0,0 +1,19 @@
+const pageThumbnailContainers = document.querySelectorAll(".page-thumbnail");
+const pageDisplay = document.querySelector("#page-display");
+const PAGE_IMAGE_ENDPOINT_PREFIX = "/page/";
+
+for (let pageThumbnailContainer of pageThumbnailContainers) {
+  pageThumbnailContainer.addEventListener("click", displayPage);
+}
+
+/* Display a page. Click handler for page thumbnails. */
+function displayPage(event) {
+  let pageThumbnailContainer = event.currentTarget;
+  pageDisplay.innerHTML = "";
+
+  let pageId = pageThumbnailContainer.dataset.pageId;
+
+  let pageImage = document.createElement("img");
+  pageImage.src = `${PAGE_IMAGE_ENDPOINT_PREFIX}/${pageId}`;
+  pageDisplay.appendChild(pageImage);
+}
