fa38ea91439c1490c589e4e772884f1556299237
Brian <mgbrian@users.noreply.github.com>
2024-10-25 21:56:17 +0100

    Update socket code to attempt to reconnect if abruptly disconnected

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,15 +1,55 @@
-const socket = new WebSocket(`ws://${window.location.host}/ws/status/`);
+const SOCKET_ENDPOINT = `ws://${window.location.host}/ws/status/`;
+let socket;
 const clientId = crypto.randomUUID(); // Identify ourselves to server.
 
-socket.onopen = () => {
-  socket.send(clientId);
-};
+const MAX_RECONNECT_ATTEMPTS = 5;
+let reconnectAttempts = 0;
 
-socket.onclose = () => {
-  console.log("WebSocket connection closed");
-};
+function connectSocket() {
+  socket = new WebSocket(SOCKET_ENDPOINT);
 
-socket.onmessage = handleServerMessage;
+  socket.onopen = () => {
+    socket.send(clientId);
+  };
+
+  socket.onclose = (event) => {
+    if (event.wasClean) {
+      console.log("WebSocket connection closed cleanly.");
+    } else {
+      console.log("WebSocket connection closed abruptly");
+      attemptReconnect();
+    }
+  };
+
+  socket.onmessage = handleServerMessage;
+
+  socket.onerror = (error) => {
+    console.error("WebSocket error:", error);
+    // Close and trigger reconnection.
+    socket.close();
+    attemptReconnect();
+  };
+}
+
+/* Attempt socket reconnection with exponential backoff. */
+function attemptReconnect() {
+  if (reconnectAttempts < MAX_RECONNECT_ATTEMPTS) {
+    reconnectAttempts++;
+    const delay = Math.pow(2, reconnectAttempts) * 1000; // ms
+
+    console.log(
+      `Attempting to reconnect... Attempt ${reconnectAttempts} in ${delay / 1000} seconds`,
+    );
+
+    setTimeout(() => {
+      connectSocket();
+    }, delay);
+  } else {
+    console.log(
+      "Max reconnection attempts reached. WebSocket will not reconnect.",
+    );
+  }
+}
 
 /* Handle a message from the server.
 
@@ -38,3 +78,5 @@ function handleServerMessage(event) {
       doc.status;
   }
 }
+
+connectSocket();
