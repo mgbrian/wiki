ab6158383b987cfddfcc3c267fe487754e50cc0e
Brian <mgbrian@users.noreply.github.com>
2024-11-05 13:18:48 +0000

    Remove additional redundant code in start script.

--- start.sh
+++ start.sh
@@ -3,13 +3,12 @@
 # Set DEBUG to 0 if not already defined.
 export DEBUG=${DEBUG:-0}
 
+# The IF part => Running in Docker Container; ELSE part => Bare Metal.
 if [ -d "/app" ]; then
-    RUNNING_ON_DOCKER=true
     LOG_DIR="/var/log"
     MEDIA_DIR="/app/media"
     VENV_DIR="/app/.requirements"
 else
-    RUNNING_ON_DOCKER=false
     LOG_DIR="./log"
     MEDIA_DIR="./media"
     VENV_DIR=".requirements"
@@ -22,12 +21,9 @@ if [ ! -d "$LOG_DIR" ]; then
 fi
 
 # Create media folder if it doesn't exist.
-if [ "$RUNNING_ON_DOCKER" = true ]; then
-    if [ ! -d "$MEDIA_DIR" ]; then
-        mkdir -p "$MEDIA_DIR"
-        echo "'media' folder created at $MEDIA_DIR."
-    fi
-
+if [ ! -d "$MEDIA_DIR" ]; then
+    mkdir -p "$MEDIA_DIR"
+    echo "'media' folder created at $MEDIA_DIR."
 fi
 
 source "$VENV_DIR"/bin/activate
