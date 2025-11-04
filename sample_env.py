adeb6cf06cc577db0706d2eae7b4435895d03a91
Brian <mgbrian@users.noreply.github.com>
2025-11-04 12:27:52 +0000

    Add GOOGLE_API_KEY to environment variables and explain Flask/Django secret keys.

--- sample_env.py
+++ sample_env.py
@@ -1,10 +1,14 @@
 """Use this to create an env.py and populate the variables below."""
 import os
 
-
+# Set this to a long random sting
 os.environ.setdefault('FLASK_SECRET_KEY', '')
 
+# API key for Gemini.
+os.environ.setdefault('GOOGLE_API_KEY', '')
+
 # DB settings.
+# Set Django secret key to a long random string.
 os.environ.setdefault('DJANGO_SECRET_KEY', '')
 os.environ.setdefault('POSTGRES_DB_NAME', '')
 os.environ.setdefault('POSTGRES_DB_USER', '')
